c39c3738681eb3619de9396213f1b6a872eac9b8
Michelle Gill <michelle@michellelynngill.com>
2017-09-15 10:49:09 -0400

    last part before questions

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -242,6 +242,11 @@ class AirCargoProblem(Problem):
         """
         # TODO implement (see Russell-Norvig Ed-3 10.2.3  or Russell-Norvig Ed-2 11.2)
         count = 0
+        node = decode_state(node.state, self.state_map)
+
+        for goal in self.goal:
+            if goal in node.neg:
+                count += 1
         return count
 
 
